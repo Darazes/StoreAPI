701afaeccd828ee4460e518c5a221257552f88f3
Даниил ᅠ <daniag2001@yandex.ru>
2021-04-30 21:30:21 +0300

    login demo

---
+++ StoreAPI/Controllers/CustomersController.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using StoreAPI.Models;
+using System.Web.Security;
+
+namespace StoreAPI.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private StoreContext db = new StoreContext();
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string login, string password)
+        {
+            if (login == "" || password == "")
+            {
+                ViewBag.error = "Заполните все поля";
+                return View();
+            }
+            // поиск пользователя в бд
+            Customer customer = null;
+
+            customer = db.Customers.FirstOrDefault(u => u.login == login && u.password == password);
+
+            
+            if (customer != null)
+            {
+                FormsAuthentication.SetAuthCookie(login, true);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.error = "Такого пользователя не существует";
+                return View();
+            }
+            
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register([Bind(Include = "id_customer,login,password,phone,adress_customer")] Customer model)
+        {
+            if (ModelState.IsValid)
+            {
+                Customer customer = null;
+
+                customer = db.Customers.FirstOrDefault(u => u.login == model.login);
+
+                if (customer == null)
+                {
+                    // создаем нового пользователя
+
+                    db.Customers.Add(model);
+                    db.SaveChanges();
+
+                    customer = db.Customers.Where(u => u.login == model.login && u.password == model.password).FirstOrDefault();
+                    
+                    // если пользователь удачно добавлен в бд
+                    if (customer != null)
+                    {
+                        FormsAuthentication.SetAuthCookie(model.login, true);
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Пользователь с таким логином уже существует");
+                }
+            }
+
+            return View(model);
+        }
+        public ActionResult Logoff()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+    
+
+    // GET: Customers
+    public async Task<ActionResult> Index()
+        {
+            return View(await db.Customers.ToListAsync());
+        }
+
+        // GET: Customers/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = await db.Customers.FindAsync(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // GET: Customers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customers/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в разделе https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "id_customer,login,password,phone,adress_customer")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Customers.Add(customer);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(customer);
+        }
+
+        // GET: Customers/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = await db.Customers.FindAsync(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: Customers/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в разделе https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "id_customer,login,password,phone,adress_customer")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(customer).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(customer);
+        }
+
+        // GET: Customers/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = await db.Customers.FindAsync(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: Customers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Customer customer = await db.Customers.FindAsync(id);
+            db.Customers.Remove(customer);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
